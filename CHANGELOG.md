6d52195f2d799d587a8438d59e9a453ef31430cb
LWenK <m@lwenk.com>
2025-03-04 15:30:40 +0800

    feat: inventory transfer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,11 +7,22 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [未发布]
 
+## [0.1.1] - 2025-03-04
+
+### 新增
+
+- 物品在容器和背包之间转移(全部/部分)
+
+### 更改
+
+- 整理背包不再整理物品栏(从背包第10格开始排序);
+
 ## [0.1.0] - 2025-03-03
 
 ### 新增
 
 - 第一次提交 
 
-[未发布]: https://github.com/LeafKnife/ContainerSorted/compare/v0.1.0...dev
+[未发布]: https://github.com/LeafKnife/ContainerSorted/compare/v0.1.1...main
+[0.1.1]: https://github.com/LeafKnife/ContainerSorted/compare/v0.1.0...v0.1.1
 [0.1.0]: https://github.com/LeafKnife/ContainerSorted/releases/tag/v0.1.0
--- src/mod/ContainerSorted.cpp
+++ src/mod/ContainerSorted.cpp
@@ -14,6 +14,8 @@
 #include <mc/world/actor/player/PlayerInventory.h>
 #include <mc/world/containers/ContainerEnumName.h>
 #include <mc/world/containers/FullContainerName.h>
+#include <mc/world/containers/managers/models/ContainerManagerModel.h>
+#include <mc/world/containers/managers/models/HudContainerManagerModel.h>
 #include <mc/world/inventory/network/ContainerScreenContext.h>
 #include <mc/world/inventory/network/ItemStackNetResult.h>
 #include <mc/world/inventory/network/ItemStackRequestActionHandler.h>
@@ -27,6 +29,8 @@
 #include <mc/world/level/BlockSource.h>
 #include <mc/world/level/block/Block.h>
 #include <mc/world/level/block/actor/BlockActor.h>
+#include <mc/world/level/block/actor/ShulkerBoxBlockActor.h>
+#include <optional>
 
 namespace lk::hook {
 
@@ -40,6 +44,64 @@ inline void sortContainer(Container& container) {
     container.removeAllItems();
     for (auto& item : items) {
         container.addItem(item);
+    }
+}
+
+inline void sortInventory(Container& container) {
+    auto items = container.getSlotCopies();
+    std::sort(items.begin() + 9, items.end(), [](ItemStack const& itemA, ItemStack const& itemB) {
+        if (itemA.getId() != itemB.getId()) return itemA.getId() > itemB.getId();
+        if (itemA.getAuxValue() != itemB.getAuxValue()) return itemA.getAuxValue() > itemB.getAuxValue();
+        return itemA.mCount > itemB.mCount;
+    });
+    container.removeAllItems();
+    for (auto& item : items) {
+        container.addItem(item);
+    }
+}
+
+inline void transferInventory2Container(Container& inventory, Container& container, bool all = true) {
+    for (auto item = inventory.begin() + 9; item != inventory.end(); ++item) {
+        if (item->isNull()) continue;
+        if (!all) {
+            if (container.getItemCount(*item) == 0) continue;
+        }
+        if (container.addItem(*item)) {
+            item->setNull(std::nullopt);
+        } else {
+            return;
+        }
+    }
+}
+
+inline void transferInventory2ShulkerBox(Container& inventory, Container& container, bool all = true) {
+    for (auto item = inventory.begin() + 9; item != inventory.end(); ++item) {
+        if (item->isNull()) continue;
+        if (!ShulkerBoxBlockActor::itemAllowed(*item)) continue;
+        if (!all) {
+            if (container.getItemCount(*item) == 0) continue;
+        }
+        if (container.addItem(*item)) {
+            item->setNull(std::nullopt);
+        } else {
+            return;
+        }
+    }
+}
+
+inline void transferContainer2Inventory(Container& inventory, Container& container, bool all = true) {
+    for (auto slot = 0; slot < container.getContainerSize(); slot++) {
+        auto& item = container[slot];
+        if (item.isNull()) continue;
+        if (!all) {
+            if (inventory.getItemCount(item) == 0) continue;
+        }
+        if (inventory.addItem(item)) {
+            item.setNull(std::nullopt);
+            container.setContainerChanged(slot);
+        } else {
+            return;
+        }
     }
 }
 
@@ -64,12 +126,12 @@ LL_TYPE_INSTANCE_HOOK(
     if (!srcContainer) return origin(requestAction, isSwap, isSrcHintSlot, isDstHintSlot);
     auto const& srcItem = srcContainer->getItem(src.mSlot);
     if (srcItem.getTypeName() != "minecraft:stick") return origin(requestAction, isSwap, isSrcHintSlot, isDstHintSlot);
+    auto inventory = mPlayer.mInventory->mInventory.get();
     switch (dst.mFullContainerName.mName) {
     case ContainerEnumName::InventoryContainer:
     case ContainerEnumName::CombinedHotbarAndInventoryContainer: {
         if (dst.mSlot != 35) return origin(requestAction, isSwap, isSrcHintSlot, isDstHintSlot);
-        auto inventory = mPlayer.mInventory->mInventory.get();
-        sortContainer(*inventory);
+        sortInventory(*inventory);
         mPlayer.refreshInventory();
         return ItemStackNetResult::Error;
     }
@@ -77,12 +139,11 @@ LL_TYPE_INSTANCE_HOOK(
     case ContainerEnumName::ShulkerBoxContainer:
     case ContainerEnumName::BarrelContainer: {
         // if (dst.mSlot != 0 && dst.mSlot != 1 && dst.mSlot != 2)
-        if (dst.mSlot != 0) return origin(requestAction, isSwap, isSrcHintSlot, isDstHintSlot);
+        //     return origin(requestAction, isSwap, isSrcHintSlot, isDstHintSlot);
         auto& screenCtx  = getScreenContext();
         auto  screenType = screenCtx.mUnk2a0ccb.as<SharedTypes::Legacy::ContainerType>();
         if (screenType != SharedTypes::Legacy::ContainerType::Container)
             return origin(requestAction, isSwap, isSrcHintSlot, isDstHintSlot);
-
         Container* ct         = nullptr;
         auto       blockActor = screenCtx.tryGetBlockActor();
         auto       actor      = screenCtx.tryGetActor();
@@ -90,14 +151,40 @@ LL_TYPE_INSTANCE_HOOK(
             Vec3 pos = actor->getPosition();
             ct       = actor->getDimensionBlockSource().tryGetContainer(BlockPos(pos));
         } else if (blockActor) {
-            if (blockActor->mBlock->getTypeName() == "minecraft:ender_chest") {
+            if (blockActor->mType == BlockActorType::EnderChest) {
                 ct = mPlayer.getEnderChestContainer();
             } else {
                 ct = blockActor->getContainer();
             }
         }
         if (!ct) return origin(requestAction, isSwap, isSrcHintSlot, isDstHintSlot);
-        sortContainer(*ct);
+        switch (dst.mSlot) {
+        case 0:
+            sortContainer(*ct);
+            break;
+        case 1:
+            if (blockActor && blockActor->mType == BlockActorType::ShulkerBox) {
+                transferInventory2ShulkerBox(*inventory, *ct);
+                break;
+            }
+            transferInventory2Container(*inventory, *ct);
+            break;
+        case 2:
+            transferContainer2Inventory(*inventory, *ct);
+            break;
+        case 3:
+            if (blockActor && blockActor->mType == BlockActorType::ShulkerBox) {
+                transferInventory2ShulkerBox(*inventory, *ct, false);
+                break;
+            }
+            transferInventory2Container(*inventory, *ct, false);
+            break;
+        case 4:
+            transferContainer2Inventory(*inventory, *ct, false);
+            break;
+        default:
+            break;
+        }
         mPlayer.refreshInventory();
         return ItemStackNetResult::Error;
     }
